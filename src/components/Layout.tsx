7c10151a4c119a8721bd605c32bdc799c1336b11
Rishabh <rishu605@gmail.com>
2025-03-08 17:07:53 +0530

    added status bar

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,20 +1,12 @@
 
+import StatusBar from "./StatusBar";
 import StoryPage from "./StoryPage";
-import { FaBatteryFull } from "react-icons/fa"; // Import battery icon
 
 function Layout() {
 
   return (
     <>
-      {/* Mobile-style status bar */}
-      <div className="status-bar">
-        <div className="time">{(new Date()).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</div>
-        {/* placeholder for time */}
-        <div className="instagram-logo">Instagram</div>
-        <div className="battery">
-          <FaBatteryFull size={20} />
-        </div>
-      </div>
+      <StatusBar/>
 
       {/* Main Content */}
       <StoryPage />
